a8c38342a2483aa6890dea4f09afb75217be9c0b
Tec-Dynamics <support@tecdynamics.co.uk>
2022-02-14 11:00:03 +0000

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Tec\SocialLogin\Facades\SocialServiceFacade;
+
+Route::group(['namespace' => 'Tec\SocialLogin\Http\Controllers', 'middleware' => ['web', 'core']], function () {
+
+    Route::group(['prefix' => BaseHelper::getAdminPrefix(), 'middleware' => 'auth'], function () {
+        Route::group(['prefix' => 'social-login'], function () {
+
+            Route::get('settings', [
+                'as'   => 'social-login.settings',
+                'uses' => 'SocialLoginController@getSettings',
+            ]);
+
+            Route::post('settings', [
+                'as'         => 'social-login.settings.post',
+                'permission' => 'social-login.settings',
+                'uses'       => 'SocialLoginController@postSettings',
+            ]);
+        });
+    });
+
+    $providers = collect(SocialServiceFacade::getProviderKeys())->implode('|');
+
+    Route::get('auth/{provider}', [
+        'as'   => 'auth.social',
+        'uses' => 'SocialLoginController@redirectToProvider',
+    ])->where('provider', $providers);
+
+    Route::get('auth/callback/{provider}', [
+        'as'   => 'auth.social.callback',
+        'uses' => 'SocialLoginController@handleProviderCallback',
+    ])->where('provider', $providers);
+
+});
---
+++ src/Http/Requests/SocialLoginRequest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tec\SocialLogin\Http\Requests;
+
+use Tec\Support\Http\Requests\Request;
+
+class SocialLoginRequest extends Request
+{
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [];
+    }
+}
